0aa0f5ed612407a9d5fe36a2795339ba3e00e445
ClaudiaPensa <92572877+ClaudiaPensa@users.noreply.github.com>
2021-10-29 11:01:32 +0200

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -14,3 +14,44 @@ install.packages("raster")
 
 library(raster)
 
+# now we import data in R
+# https://www.rdocumentation.org/packages/raster/versions/3.4-13/topics/brick 
+# we use the function brick 
+
+# first set the working directory 
+# setwd("/Users/name/lab/") # mac
+
+setwd("/Users/account2/Desktop/lab/")
+
+# grd graphic file and 
+# gri index file 
+
+# we are going to import satellite data
+# l for landsat satellite (bject in R can't be number)
+l2011 <- brick("p224r63_2011.grd")
+
+l2011
+
+plot(l2011)
+
+# B1 is the reflectance in the blue band 
+# B2 is the reflectance in the green band 
+# B3 is the reflectance in the red band 
+
+# We can change the color palette
+# https://www.rdocumentation.org/packages/dichromat/versions/1.1/topics/colorRampPalette
+# black correspond to the lowelest value 
+# ( black, grey,...) is an array so we write c before the parentesies, 100 means how many tones we use
+cl <- colorRampPalette(c("black", "grey", "light grey"))(100)
+plot(l2011, col=cl) 
+
+# We maching the bands in the RGB components 
+# https://www.rdocumentation.org/packages/raster/versions/3.5-2/topics/plotRGB
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+# the result is a natural color image 
+
+
+
+
+
