bcda9debfefc25d59d43791729cead903f101eaf
ClaudiaPensa <92572877+ClaudiaPensa@users.noreply.github.com>
2021-11-08 10:56:20 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -29,6 +29,7 @@ setwd("/Users/account2/Desktop/lab/")
 # we are going to import satellite data
 # l for landsat satellite (object in R can't be number)
 # l2011 is a raster brick file 
+# the brick function imports all the layers
 l2011 <- brick("p224r63_2011.grd")
 
 l2011
@@ -53,7 +54,7 @@ plot(l2011, col=cl)
 plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
 # the result is a natural color image 
 
-# We wanto to plot each band (for eg. the bend n1 called B1_sre (spectral reflectance) and so on.. )
+# We want to plot each band (for eg. the bend n1 called B1_sre (spectral reflectance) and so on.. )
 # Let's plot the green band 
 # We link the dataset (l2011) with the band (B2_sre) with the symbol"$"
 # plot(l2011 $ B2_sre) where: "plot" is the function and the "parentheses" is the argument
@@ -85,4 +86,68 @@ par(mfrow=c(2,1)) #the first number is the number of rows in the multiframe, whi
 plot(l2011$B1_sre, col=clb)
 plot(l2011$B2_sre, col=clg)
 
+#---- day 3 recap
+# plot only the blue band 
+plot(l2011$B1_sre)
+# plot only the blue band using a blue colorRampPalette
+clb <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+
+# plot both images and creating a multiframe
+# plot the blue and the green band besides with the funcion "par", with different colorRampPalette
+clg <- colorRampPalette(c("dark green", "green", "light green"))(100)
+plot(l2011$B2_sre, col=clg)
+par(mfrow=c(1,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+# Exercise: put the plots one on top of the other (blue band on top, green band at bottom)
+# invert the number of rows and the number of columns 
+par(mfrow=c(2,1)) # two rows and just one column
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+# Exercise : plot the first four bands with two rows and two columns
+# the order is (from left to right) B1 , B2 , B3 , B4
+# the colorRampPalette for the the nir is "red","orange","yellow" 
+
+# First define the colorRampPalette for the B3 (red band)
+#B3
+clr <- colorRampPalette(c("dark red", "red", "pink"))(100)
+plot(l2011$B3_sre, col=clr)
+
+# Do the same for the B4 band (nir band) 
+#B4
+clnir <- colorRampPalette(c("red", "orange", "yellow"))(100)
+plot(l2011$B4_sre, col=clnir)
+
+par(mfrow=c(2,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+plot(l2011$B3_sre, col=clr)
+plot(l2011$B4_sre, col=clnir)
+
+# plot rgb function https://www.rdocumentation.org/packages/raster/versions/3.5-2/topics/plotRGB
+# first function to clean dev.off() function that close the plotting device ( the windows) 
+dev.off()
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin") #real colours
+# the result image is in a visible spectre 
+
+# now we want to extend this in the nir spectre but where put the B4? 
+# change the composition to r = B4, g = B3 , b = B2 (shift by one)
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") #false colours
+# vegetation is red bcs in the nir is the band where vegetations reflect a lot 
+
+# now change the nir component and put it in the green component
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin") 
+
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
+# all the parts in yellow are areas where there has been the distribution of forests 
+
+# plot all the images togheter 
+par(mfrow=c(2,2))
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin") #real colours
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") #false colours
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin") #false colours
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin") #false colours
 
