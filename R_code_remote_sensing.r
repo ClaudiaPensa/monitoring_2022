2d8801295d6f9b429ff410d22c0bcc243869973d
ClaudiaPensa <92572877+ClaudiaPensa@users.noreply.github.com>
2021-11-12 10:08:19 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -29,7 +29,7 @@ setwd("/Users/account2/Desktop/lab/")
 # we are going to import satellite data
 # l for landsat satellite (object in R can't be number)
 # l2011 is a raster brick file 
-# the brick function imports all the layers
+# the brick function imports the files into R
 l2011 <- brick("p224r63_2011.grd")
 
 l2011
@@ -147,7 +147,24 @@ plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
 # plot all the images togheter 
 par(mfrow=c(2,2))
 plotRGB(l2011, r=3, g=2, b=1, stretch="Lin") #real colours
-plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") #false colours
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") #false colours 
 plotRGB(l2011, r=3, g=4, b=2, stretch="Lin") #false colours
 plotRGB(l2011, r=3, g=2, b=4, stretch="Lin") #false colours
+
+# Final day on this tropical forest reserve
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") #the nir is now red and is the best solution in which plants absorb the blue and red, reflect green and the nir. 
+plotRGB(l2011, r=4, g=3, b=2, stretch="Hist") #histogram stretching that enounce the stream 
+
+#now we import the data of 1988 with brick function
+l1988 <- brick("p224r63_1988.grd")
+l1988 # always with 7 bands, the same resolution of l2011
+# plot together the 2 images with the par function 
+par(mfrow=c(2,1)) #2 rows and 1 column where the first row is the 1988 image, the second is the 2011 image 
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+
+# Put the nir in the blue channel to enounce the agricultural area 
+par(mfrow=c(2,1))
+plotRGB(l1988, r=2, g=3, b=4, stretch="Lin")
+plotRGB(l2011, r=2, g=3, b=4, stretch="Lin")
 
