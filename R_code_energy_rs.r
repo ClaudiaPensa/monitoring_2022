b1e183c709d753ce09a3443961082b44761bcfce
ClaudiaPensa <92572877+ClaudiaPensa@users.noreply.github.com>
2021-11-19 11:06:56 +0100

    Update R_code_energy_rs.r

--- R_code_energy_rs.r
+++ R_code_energy_rs.r
@@ -6,10 +6,13 @@ library(raster)
 # set the working directory 
 setwd("/Users/account2/Desktop/lab/")
 
+
 # Import the data from the lab folder with the brick function
 l1992 <- brick("defor1_.jpg") # image of 1992
 l2006 <- brick("defor2_.jpg") # image of 2006
 
+library(rgdal)
+l1992 <- brick("defor1_.jpg") # image of 1992
 l1992
 plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
 plotRGB(l1992, r=2, g=1, b=3, stretch="Lin")
