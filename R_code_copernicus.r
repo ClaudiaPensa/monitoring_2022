a479e83f9a4598f3fed60c948350a1dfc63bb806
ClaudiaPensa <92572877+ClaudiaPensa@users.noreply.github.com>
2021-12-17 09:16:18 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -12,3 +12,27 @@ soilwaterindex20211211
 
 plot(soilwaterindex20211211)
 
+##### day 2 #####
+# recalling the libraries in R and setting the working directory 
+
+
+# colorRampPalette to see the snow coverage at higher latitues 
+cl <- colorRampPalette (c('dark blue','blue','light blue'))(100)
+plot(soilwaterindex20211211, col=cl)
+
+
+# viridis package used to change the colour
+# it directly uses a series of colours 
+# The package contains eight color scales: “viridis”, the primary choice, and five alternatives with similar properties
+install.packages("viridis")
+library(viridis)
+library(RStoolbox) 
+library(ggplot2)
+
+# ggplot and add geom raster, the geometry we want to use (e.g. histograms) 
+ggplot() + geom_raster(soilwaterindex20211211, mapping = aes(x=x, y=y, fill=Surface.State.Flag))
+# ggplot with viridis
+# scale_fill_viridis function is the function we use to change the colours 
+
+ggplot() +geom_raster(soilwaterindex20211211, mapping = aes(x=x, y=y, fill=Surface.State.Flag )) + scale_fill_viridis(option="cividis") + ggtitle("cividis palette")
+
